e9ad64cf7048af6e1c9c024288dea247d2c0eb09
policarpofelipe <policarpofelipe@gmail.com>
2026-03-04 15:07:15 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import univespLogo from "./imagens/Univesp_logo.jpg";
+import univespLogo from "./assets/imagens/Univesp_logo.jpg";
 import "./App.css";
 
 export default function App() {
