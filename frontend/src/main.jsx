a5c95ebadf136f603ae67ae16ce591810b0bae8e
policarpofelipe <policarpofelipe@gmail.com>
2026-04-03 22:00:03 -0300

    Atualizar o main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,17 +7,10 @@ import "./styles/tokens.css";
 import "./styles/themes.css";
 import "./styles/globals.css";
 
-import { AccessibilityProvider } from "./context/AccessibilityContext";
-import { AuthProvider } from "./context/AuthContext";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AccessibilityProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </AccessibilityProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
